9b7d06fcea763fb1abc414f2743ae962159db017
MilIeneum <milieneum@yandex.ru>
2022-12-02 12:56:50 +0300

    Initial Commit

---
+++ Conway's Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Conway's Game
+//
+//  Created by Michael on 12/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
